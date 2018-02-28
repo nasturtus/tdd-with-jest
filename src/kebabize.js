4ab4f8bfe77e587796d91f364cbd5b6ceab3efd8
nasturtus <nasturtus@gmail.com>
2018-02-28 14:52:44 +0800

    Pass two tests.

--- src/kebabize.js
+++ src/kebabize.js
@@ -8,7 +8,26 @@ kebabize('camelsHave3Humps') // camels-have-humps
 */
 
 function kebabize(input) {
-    
+  var upperCaseChrPos = [];
+
+  for (var i = 0; i < input.length; i++) {
+    //find positions for upper case
+    if (input[i].charCodeAt(0) >= 65 && input[i].charCodeAt(0) <= 90) {
+      upperCaseChrPos.push(i);
+    }
+  }
+
+  var strings = [];
+  var start = 0;
+
+  for (var i = 0; i < upperCaseChrPos.length; i++) {
+    var end = upperCaseChrPos[i];
+    strings.push(input.slice(start, end));
+    start = end;
+  }
+  strings.push(input.slice(start));
+
+  return strings.join("-").toLowerCase();
 }
 
-module.exports = kebabize
+module.exports = kebabize;
--- test/kebabize.test.js
+++ test/kebabize.test.js
@@ -1,10 +1,10 @@
 const kebabize = require("../src/kebabize");
 
-test.skip("hiThere should be kebabized to hi-there", function() {
+test("hiThere should be kebabized to hi-there", function() {
   expect(kebabize("hiThere")).toEqual("hi-there");
 });
 
-test.skip("hiThereBruceWayne shuld be kebabized to hi-there-bruce-wayne", function() {
+test("hiThereBruceWayne shuld be kebabized to hi-there-bruce-wayne", function() {
   expect(kebabize("hiThereBruceWayne")).toEqual("hi-there-bruce-wayne");
 });
 
